ecc364a1c90c7ab6ff469eba6eddea3163d8f872
solar <ghcaiminghui@163.com>
2019-01-07 13:48:18 +0800

    zuixin

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404页面</title>
+    <!-- Standard iPhone -->
+<link rel="apple-touch-icon" sizes="57x57" href="https://static.h-ui.net/www/home/images/touch-icon-iphone-114.png" />
+<!-- Retina iPhone -->
+<link rel="apple-touch-icon" sizes="114x114" href="https://static.h-ui.net/www/home/images/touch-icon-iphone-114.png" />
+<!-- Standard iPad -->
+<link rel="apple-touch-icon" sizes="72x72" href="https://static.h-ui.net/www/home/images/touch-icon-ipad-144.png" />
+<!-- Retina iPad -->
+<link rel="apple-touch-icon" sizes="144x144" href="https://static.h-ui.net/www/home/images/touch-icon-ipad-144.png" />
+<!--[if lt IE 9]>
+<script type="text/javascript" src="https://lib.h-ui.net/html5.js"></script>
+<script type="text/javascript" src="https://lib.h-ui.net/respond.min.js"></script>
+<![endif]-->
+<link rel="stylesheet" type="text/css" href="https://static.h-ui.net/h-ui/3.1/css/H-ui.min.css" />
+<link rel="stylesheet" type="text/css" href="https://static.h-ui.net/h-ui.admin/3.1/css/H-ui.admin.css" />
+<link rel="stylesheet" type="text/css" href="https://static.h-ui.net/www/home/css/H-ui.doc.css" />
+<link rel="stylesheet" type="text/css" href="https://lib.h-ui.net/Hui-iconfont/1.0.9/iconfont.min.css" />
+</head>
+<body >
+<article class="page-404 minWP text-c">
+    <p class="error-title"><i class="Hui-iconfont va-m">&#xe688;</i>
+        <span class="va-m"> 404</span>
+    </p>
+    <p class="error-description">不好意思，您访问的页面不存在~</p>
+    <p class="error-info">您可以：
+        <a href="javascript:;" onclick="history.go(-1)" class="c-primary">&lt; 返回上一页</a>
+        <span class="ml-20">|</span>
+        <a href="/home/index/index" class="c-primary ml-20">去首页 &gt;</a>
+    </p>
+</article>
+</body>
+</html>
